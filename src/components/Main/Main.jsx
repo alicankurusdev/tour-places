8a53d4f192dc2853860c62b583571926231e8356
Alican Kurus <alicankurus.dev@gmail.com>
2023-08-14 11:38:18 +0300

    ready mediqery missing

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import "./Main.scss"
+import React from "react";
+import "./Main.scss";
 
-const Main = () => {
+const Main = ({ data }) => {
   return (
-    <div>Main</div>
-  )
-}
+    <div className="card-container">
+      {data.map((item) => {
+        const { id, title, desc, image } = item;
+        return (
+          <div key={id} className="card">
+            <div className="title"><h2>{title}</h2></div>
+            <div className="img-container">
+            <div className="img-filter"></div>
+                <div className="img-box">
+                <img src={image} alt="img" />
+                </div>
+                
+            </div>
+            <div className="img-filter"></div>
+            <div className="card-info">
+              <p>{desc}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Main
+export default Main;
