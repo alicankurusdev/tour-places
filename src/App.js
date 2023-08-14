0753698401af3f60ba232b76440ce02e787d90b9
Alican Kurus <alicankurus.dev@gmail.com>
2023-08-14 11:38:53 +0300

    x

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import "./App.scss"
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header"
 import Main from "./components/Main/Main"
+import {data} from "./utils/data"
 function App() {
   return (
     <div className="app">
       <Navbar/>
       <Header/>
-      <Main/>
+      <Main data={data}/>
 
 
     </div>
