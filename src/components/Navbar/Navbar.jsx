298a4df503c89117578c3ff07bbcfabda12c68d1
Alican Kurus <alicankurus.dev@gmail.com>
2023-08-14 09:59:18 +0300

    save

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Navbar.scss"
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <ul>
+        <li>ABOUT US</li>
+        <li>FOR YOU</li>
+        <li>SERVICES</li>
+        <li>BLOG</li>
+        <li>VLOG</li>
+        <li>CONTACT</li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default Navbar
